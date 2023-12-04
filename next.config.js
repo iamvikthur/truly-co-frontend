9c393cffd9e23f60a01d67af89e1af7cc48a0f0a
Anibe <samanibe@yahoo.com>
2023-12-04 14:53:28 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const withTM = require('next-transpile-modules')(['drei', 'three', 'postprocessi
 
 module.exports = withTM({
   env: {
-    BASE_URL: 'https://bytruly.co',
+    BASE_URL: 'https://truly-2vbq.onrender.com',
   },
 });
