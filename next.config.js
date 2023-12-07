b1202a72eed59c441493693f87ad18aab2872d77
Anibe Samuel <samanibe@yahoo.com>
2023-12-07 12:59:33 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,8 +4,13 @@ module.exports = withTM({
   env: {
     BASE_URL: 'https://truly-2vbq.onrender.com',
   },
-
-  future: {
-    webpack5: true,
-  },
+future: {
+        webpack5: true
+    },
+    webpack: function (config, options) {
+        console.log(options.webpack.version); // 5.18.0
+        config.experiments = {};
+        return config;
+    }
+  
 });
