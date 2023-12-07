84321be672d736dbd24cbc617e9487134c9c2353
Anibe <samanibe@yahoo.com>
2023-12-07 17:42:25 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-const withTM = require('next-transpile-modules')(['drei', 'three', 'postprocessing']);
+const withTM = require('next-transpile-modules')(['drei', 'three', 'postprocessing'], {
+  debug: true, // Enables debugging output
+});
 
 module.exports = withTM({
   env: {
-    BASE_URL: 'https://truly-2vbq.onrender.com',
+    BASE_URL: 'https://truly-2vbq.onrender.com/',
   },
-  productionSourceMaps: true,
 });
