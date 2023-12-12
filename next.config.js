f1709c08e6e0857f3bc4ee58e29d3944364e502a
Anibe <samanibe@yahoo.com>
2023-12-12 20:47:38 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -3,11 +3,31 @@ const withTM = require('next-transpile-modules')(['drei', 'three', 'postprocessi
 });
 
 module.exports = withTM({
+
+  async headers() {
+    return [
+      {
+        source: '/api/stories',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET',
+          },
+        ],
+      },
+      
+      
+    ];
+  },
   env: {
     BASE_URL: 'http://143.244.178.155',
   },
 images: {
-    domains: ['http://143.244.178.155'], // Add your base URL here
+    domains: ['http://143.244.178.155', 'http://localhost:3000'], // Add your base URL here
   },
   webpack: (config) => {
     config.module.rules.push({
