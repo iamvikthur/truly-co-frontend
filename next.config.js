ed78e2f30c56538fc9a757b5403acdea561adc30
Shigaba Victor <30593511+iamvikthur@users.noreply.github.com>
2023-12-18 22:01:26 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@ const withTM = require('next-transpile-modules')(['drei', 'three', 'postprocessi
 });
 
 module.exports = withTM({
-
-  
-  
-images: {
-    domains: ['https://www.trulyco.app', 'https://admin.trulyco.app/'], // Add your base URL here
- 
-},
+  env: {
+    BASE_URL: 'https://admin.trulyco.app',
+  },
+  images: {
+      domains: ['https://www.trulyco.app', 'https://admin.trulyco.app/'], // Add your base URL here
+   
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /react-spring/,
