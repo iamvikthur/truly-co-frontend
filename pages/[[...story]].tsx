829e493a042ec89070673351636ac1cc82b77635
Shigaba Victor <30593511+iamvikthur@users.noreply.github.com>
2023-12-20 12:12:47 +0000

    Update [[...story]].tsx

--- pages/[[...story]].tsx
+++ pages/[[...story]].tsx
@@ -19,6 +19,8 @@ const StoryPage = ({ data }) => {
   const [, setBrand] = useContext(BrandContext);
   useEffect(() => void (story === null && setBrand(null)), [story, setBrand]);
 
+  console.log("ROOT STORY COMPONENT", data);
+
   return (
     <>
       {data ? (
