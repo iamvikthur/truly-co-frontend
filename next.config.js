48d0afde27807f22e8f090603443c88b849ca149
Shigaba Victor <30593511+iamvikthur@users.noreply.github.com>
2023-12-20 12:42:38 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,25 @@ const withTM = require('next-transpile-modules')(['drei', 'three', 'postprocessi
 });
 
 module.exports = withTM({
+  async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    },
   env: {
     BASE_URL: 'https://admin.trulyco.app',
   },
   images: {
-      domains: ['https://www.trulyco.app', 'https://admin.trulyco.app'], // Add your base URL here
+      domains: ['https://www.trulyco.app', 'https://admin.trulyco.app', 'admin.trulyco.app'], // Add your base URL here
    
   },
   webpack: (config) => {
