cb7be9bce83045f5e23c20ca409969a5ac60c9a4
Shigaba Victor <30593511+iamvikthur@users.noreply.github.com>
2023-12-24 10:28:12 +0000

    Update SpaceStory.tsx

--- components/space/SpaceStory.tsx
+++ components/space/SpaceStory.tsx
@@ -26,8 +26,8 @@ const SpaceStory = ({ position = [0, 0, 0], story, onClick, onHover }: SpaceStor
 
   const args = useMemo(() => {
     const loader = new THREE.TextureLoader();
-    const uvRate = new THREE.Vector2(1, 1);
     loader.crossOrigin = '*';
+    const uvRate = new THREE.Vector2(1, 1);
     const image = loader.load(story.storyImage, (texture) => {
       texture.minFilter = THREE.LinearFilter;
       setUV(uvRate, texture);
