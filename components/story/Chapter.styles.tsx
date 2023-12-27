9f697ce4aa6301ba56bee08318ee62f179419c7f
Shigaba Victor <30593511+iamvikthur@users.noreply.github.com>
2023-12-27 19:57:55 +0000

    Update Chapter.styles.tsx

--- components/story/Chapter.styles.tsx
+++ components/story/Chapter.styles.tsx
@@ -56,7 +56,7 @@ export const CallToSwipe = styled(({ ...rest }) => (
   padding: 0 10px 0 26px;
 
   p {
-    font-family: Inter, 'Open Sans Condensed', sans-serif;
+    font-family: Inter, 'Open Sans', sans-serif;
     font-weight: 700;
     font-size: 16px;
     line-height: 28px;
